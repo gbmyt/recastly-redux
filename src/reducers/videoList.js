ecbee283c80db0f039f47c85112a44244fc4908c
gbmyt <gbmytabc1@gmail.com>
2022-10-26 22:39:29 -0400

    Complete connecting combine reducer to store

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,7 +1,12 @@
 import Redux from 'redux';
 
-var videoListReducer = (state = sampleData, action) => {
+var videoListReducer = (state = [], action) => {
   //TODO: define a reducer for the videoList field of our state.
+  switch (action.type) {
+    case 'CHANGE_VIDEO_LIST':
+      return state.concat(action.videoList); // look more into this syntax later
+    default: return state;
+  }
 };
 
 export default videoListReducer;
